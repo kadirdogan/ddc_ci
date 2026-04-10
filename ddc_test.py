cebb8f50bb7e611676e43c1777b68f88d6f7c17f
kadir <dogankadir@me.com>
2026-04-10 21:43:25 +0300

    chore: move test files to scratch

--- ddc_test.py
+++
@@ -1,5 +0,0 @@
-from monitorcontrol import get_monitors
-for monitor in get_monitors():
-    with monitor:
-        caps = monitor.get_vcp_capabilities()
-        print(caps)
--- hid_test.py
+++
@@ -1,9 +0,0 @@
-import hid, time
-dev = hid.device()
-dev.open(0x1209, 0xC55D)
-dev.set_nonblocking(False)
-print('basliyor')
-while True:
-    data = dev.read(64, timeout_ms=1000)
-    if data:
-        print(data[0], data[1])
